e80971469ba54eacbc1749c8e80c70ef5ff85444
Your Name <you@example.com>
2026-05-11 12:45:54 -0300

    Agregado microservicio ms-plans

---
+++ ms-plans/src/main/java/duoc/cl/ms_plans/MsPlansApplication.java
@@ -0,0 +1,13 @@
+package duoc.cl.ms_plans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPlansApplication.class, args);
+	}
+
+}
---
+++ ms-plans/src/test/java/duoc/cl/ms_plans/MsPlansApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.cl.ms_plans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPlansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

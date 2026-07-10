8adafa849bb408fca9d975efb7fe3c434826f118
Eurquieta <ed.urquieta@duocuc.cl>
2026-07-10 17:26:08 -0400

    se agregan dependencias para correccion de error en tests

--- ms-reservations/src/test/java/cl/duoc/ms_reservas/MsReservasApplicationTests.java
+++ ms-reservations/src/test/java/cl/duoc/ms_reservas/MsReservasApplicationTests.java
@@ -2,8 +2,10 @@ package cl.duoc.ms_reservas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsReservasApplicationTests {
 
 	@Test
